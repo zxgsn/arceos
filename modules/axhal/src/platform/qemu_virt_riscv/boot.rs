20835af3a960cadf85d32bf52b24c0fc9e135a61
Luoyuan Xiao <xiaoluoyuan@163.com>
2023-07-18 13:57:08 +0800

    cv1811 board run into shell

--- modules/axhal/src/platform/qemu_virt_riscv/boot.rs
+++ modules/axhal/src/platform/qemu_virt_riscv/boot.rs
@@ -47,9 +47,9 @@ static mut BOOT_PT_SV39: [u64; 512] = [0; 512];
 
 unsafe fn init_boot_page_table() {
     // 0x8000_0000..0xc000_0000, VRWX_GAD, 1G block
-    BOOT_PT_SV39[2] = (0x80000 << 10) | 0xef;
+    BOOT_PT_SV39[2] = (0x80000 << 10) | (0x7 << 60) | 0xef;
     // 0xffff_ffc0_8000_0000..0xffff_ffc0_c000_0000, VRWX_GAD, 1G block
-    BOOT_PT_SV39[0x102] = (0x80000 << 10) | 0xef;
+    BOOT_PT_SV39[0x102] = (0x80000 << 10) | (0x7 << 60) | 0xef;
 }
 
 unsafe fn init_mmu() {
--- scripts/make/cv1811.mk
+++ scripts/make/cv1811.mk
@@ -1,4 +1,5 @@
 cv1811: build
 	cp $(OUT_BIN) cv1811h/arceos-cv1811.bin
 	mkimage -f cv1811h/arceos-cv1811h.its cv1811h/arceos-cv1811.itb
+	@cp cv1811h/arceos-cv1811.itb /srv/tftp/
 	@echo 'Built the FIT-uImage cv1811h/arceos-cv1811.itb'
